c2665dfd49aaf3ae286524b43c26be2253418e0e
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-24 11:58:26 +0700

    function api

---
+++ CoreCodeCamp/Controllers/OperationsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public OperationsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public ActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)configuration;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error");
+            }
+        }
+    }
+}
