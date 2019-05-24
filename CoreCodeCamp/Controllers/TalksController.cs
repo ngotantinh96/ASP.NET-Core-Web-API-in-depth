6b35deaee6bf3eb7ef828f9adf18a42ac8409d06
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-24 10:37:48 +0700

    get individual talk

--- CoreCodeCamp/Controllers/TalksController.cs
+++ CoreCodeCamp/Controllers/TalksController.cs
@@ -37,5 +37,19 @@ namespace CoreCodeCamp.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, "Database Error");
             }
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<TalkModel>> Get(string moniker, int id)
+        {
+            try
+            {
+                var talk = await campRepository.GetTalkByMonikerAsync(moniker, id);
+                return mapper.Map<TalkModel>(talk);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Error");
+            }
+        }
     }
 }
