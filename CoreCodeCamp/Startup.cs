d89d9aafac47ce12c9c221e4bbd9b026c84854f3
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-24 13:55:54 +0700

    url versioning ex: api/v2/controllername

--- CoreCodeCamp/Startup.cs
+++ CoreCodeCamp/Startup.cs
@@ -27,9 +27,10 @@ namespace CoreCodeCamp
                 opt.AssumeDefaultVersionWhenUnspecified = true;
                 opt.DefaultApiVersion = new ApiVersion(1, 1);
                 opt.ReportApiVersions = true;
-                opt.ApiVersionReader = ApiVersionReader.Combine(
-                    new HeaderApiVersionReader("X-Version"),
-                    new QueryStringApiVersionReader("ver"));
+                opt.ApiVersionReader = new UrlSegmentApiVersionReader();
+                //opt.ApiVersionReader = ApiVersionReader.Combine(
+                //    new HeaderApiVersionReader("X-Version"),
+                //    new QueryStringApiVersionReader("ver"));
             });
 
             services.AddMvc(opt => opt.EnableEndpointRouting = false)
